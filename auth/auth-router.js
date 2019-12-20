192c814865f82c825cee45c6775fe1ba7021ee4b
Brad <brad@viziworx.com>
2019-12-20 11:19:37 -0500

    setup login endpoint to verify password using bcrypt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 router.post('/register', (req, res) => {
-  // implement registration
+
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      // check that passwords match
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        // return 401 if the password or username are invalid
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 module.exports = router;
