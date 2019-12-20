15972c7d6cbb76d4f84e3f5e983abdab91104c2b
Brad <brad@viziworx.com>
2019-12-20 12:05:53 -0500

    added jsonwebtoken dependency

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model');
+const generateToken = require('./token-middleware');
 
 router.post('/register', (req, res) => {
   const credentials = req.body;
@@ -28,7 +29,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       // check that passwords match
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+        const token = generateToken(user)
+        res.status(200).json({ message: `Welcome ${user.username}!`, token });
       } else {
         // return 401 if the password or username are invalid
         res.status(401).json({ message: 'Invalid credentials' });
@@ -38,5 +40,6 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,4 +1,4 @@
-/* 
+/*
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
