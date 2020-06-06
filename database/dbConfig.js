943b9b5a13348a622eb687bcff67400172d367a1
Brad <brad@viziworx.com>
2020-06-06 16:00:04 -0400

    adds ternary operator for production and development

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,7 +1,6 @@
 const knex = require('knex');
-
 const knexConfig = require('../knexfile.js');
 
-const environment = process.env.DB_ENV;
+const environment = process.env.DB_ENV === 'production' ? 'production' : 'development';
 
 module.exports = knex(knexConfig[environment]);
