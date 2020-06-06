cdc078837664a87aea552def33a15ffc0adc026e
Brad <brad@viziworx.com>
2020-06-06 15:46:00 -0400

    adds morgan and cleans up code-formatting

--- api/server.js
+++ api/server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const morgan = require('morgan');
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+const authRouter = require('../auth/router');
+const jokesRouter = require('../jokes/router');
+const { authenticate } = require('../auth/authenticate');
 
-const server = express();
-
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
-
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+const server = express()
+  .use(helmet())
+  .use(cors())
+  .use(express.json())
+  .use(morgan('dev'))
+  .use('/api', authRouter)
+  .use('/api/jokes', authenticate, jokesRouter);
 
 module.exports = server;
