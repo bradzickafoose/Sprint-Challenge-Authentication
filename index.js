2da47afd652b0c6519ef7a4ff2bf935a5f100ca3
Brad <brad@viziworx.com>
2020-06-06 15:44:40 -0400

    changes fallback port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-const server = require('./api/server.js');
+const server = require('./api/server');
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 3300;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
